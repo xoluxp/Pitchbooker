0242030ae48f00b8f8ee126f98f3e65aede82295
xoluxp <xoluxp@gmail.com>
2025-01-07 03:13:01 +0100

    add routing for signup and login; implement Signup and Login components with form handling

--- pitchbook/pitchbook-frontend/src/components/Hero.js
+++ pitchbook/pitchbook-frontend/src/components/Hero.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Hero.css';
+import { Link } from 'react-router-dom'
 
 function Hero() {
   return (
@@ -8,8 +9,12 @@ function Hero() {
         <div className="hero-text">
           <h1 className="hero-heading">The Pitch Book</h1>
           <p className="hero-subheading">Book your football pitch with ease</p>
+          <Link to="pitchbook\pitchbook-frontend\src\components\Signup.js"> {/* Link to Signup page */}
           <button className="btn btn-primary">SIGN UP NOW</button>
+          </Link>
+          <Link to="pitchbook\pitchbook-frontend\src\components\Login.js"> {/* Link to Login page */}
           <button className="btn btn-secondary">LOG IN</button>
+          </Link>
         </div>
         <div className="hero-graphic">
           {/* Add your graphic here */}
